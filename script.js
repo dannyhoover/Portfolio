751e8f5956d527de08930604670bd31d1c180bd4
dannyhoover <dofjrtd@gmail.com>
2020-09-03 17:26:06 -0700

    Post-tutoring style updates
    Co-authored-by: Wesley Clements <wesley.clements@gmail.com>

--- script.js
+++ script.js
@@ -44,25 +44,32 @@ function displayAboutPage() {
     document.getElementById("nav-item-about").className = "nav-item active";
     document.getElementById("nav-item-portfolio").className = "nav-item";
     document.getElementById("nav-item-contact").className = "nav-item";
-    const aboutMeRow1 = $("<div>").addClass("row").appendTo(aboutPage);
-    const aboutMeCol = $("<div>").addClass("col-md-3").appendTo(aboutMeRow1);
-    const aboutMeText = $("<h1>").text("About Me").appendTo(aboutMeCol);
 
     // about me block
-    const aboutMeRow2 = $("<div>").addClass("row").appendTo(aboutPage);
-    const welcomeTextCol = $("<div>").addClass("col-md-7").appendTo(aboutMeRow2);
-    const welcomeTextBodyP1 = $("<p>").text("Welcome to my page! My name is Danny Hoover. My favorite things include being a dad, playing video games, and listening to audiobooks. In my picture you'll see my amazing wife Ashley and our daughter Emma. These two are my world.").appendTo(welcomeTextCol);
-    const welcomeTextBodyP2 = $("<p>").text("I have a bachelors degree in Sociology from the University of Oregon, my background is in finance and logistics, and current events have led me to pursue a career in coding!").appendTo(welcomeTextCol);
-    const welcomeTextBodyP3 = $("<p>").text("Here you'll find some of my projects resulting from this new endeavor!").appendTo(welcomeTextCol);
-    const welcomeTextBodyP4 = $("<p>").text("My Links:").appendTo(welcomeTextCol);
-    const welcomeTextBodyP5 = $("<p>").appendTo(welcomeTextCol);
-    const myGithubLink = $("<a>Github</a>").attr("href", "https://github.com/dannyhoover").attr("target", "_blank").appendTo(welcomeTextBodyP5);
-    const welcomeTextBodyP6 = $("<p>").appendTo(welcomeTextCol);
-    const myLinkedInLink = $("<a>LinkedIn</a>").attr("href", "https://www.linkedin.com/in/daniel-hoover-269278172/").attr("target", "_blank").appendTo(welcomeTextBodyP6);
-
-    // picture block
-    const famPicDiv = $("<div>").addClass("col-md-5").appendTo(aboutMeRow2);
-    const famPicture = $("<img>").attr("src", "https://scontent.fhio2-1.fna.fbcdn.net/v/t1.0-9/60346112_10219121352135738_9049716716552060928_n.jpg?_nc_cat=103&_nc_sid=8bfeb9&_nc_ohc=hCW4q79rW04AX_pD-R3&_nc_ht=scontent.fhio2-1.fna&oh=9f2421e530d25db74922a5da3e3a920a&oe=5F7241D0").attr("alt", "thumbnail").addClass("img-thumbnail").appendTo(famPicDiv);
+    aboutPage.append(
+    `<div class="row">
+        <div class="col-md-3">
+            <h1>About Me</h1>
+        </div>
+    </div>`,
+    `<div class="row">
+        <div class="col-md-7">
+            <p>Welcome to my page! My name is Danny Hoover. My favorite things include being a dad, playing video games, and listening to audiobooks. In my picture you'll see my amazing wife Ashley and our daughter Emma. These two are my world.</p>
+            <p>I have a bachelors degree in Sociology from the University of Oregon, my background is in finance and logistics, and current events have led me to pursue a career in coding!</p>
+            <p>Here you'll find some of my projects resulting from this new endeavor!</p>
+            <p>My Links:</p>
+            <p><a href="https://github.com/dannyhoover" target="_blank">Github</a></p>
+            <p><a href="https://www.linkedin.com/in/daniel-hoover-269278172/" target="_blank">LinkedIn</a></p>
+        </div>
+        <div class="col-md-5">
+            <img
+                class="img-thumbnail"
+                src="https://scontent.fhio2-1.fna.fbcdn.net/v/t1.0-9/60346112_10219121352135738_9049716716552060928_n.jpg?_nc_cat=103&_nc_sid=8bfeb9&_nc_ohc=hCW4q79rW04AX_pD-R3&_nc_ht=scontent.fhio2-1.fna&oh=9f2421e530d25db74922a5da3e3a920a&oe=5F7241D0"
+                alt="thumbnail"
+            />
+        </div>
+    </div>`
+    );
 }
 
 // portfolio page function
@@ -75,8 +82,9 @@ function displayPortfolioPage() {
     // jumbotron for the top of the page underneath header
     const jumbotronDisplay = $("<div>").addClass("jumbotron jumbotron-fluid").appendTo(portfolioPage);
     const jumboContainer = $("<div>").addClass("container").appendTo(jumbotronDisplay);
-    const jumboText1 = $("<h1>").addClass("display-6").text("My Portfolio").appendTo(jumboContainer);
-    const jumboText2 = $("<p>").addClass("lead").text("These are a few of my works so far. Click the images to take a look!").appendTo(jumboContainer);
+    const jumboText1 = $("<h1>").addClass("display-6").text("My Portfolio");
+    const jumboText2 = $("<p>").addClass("lead").text("These are a few of my works so far. Click the images to take a look!");
+    jumboContainer.append(jumboText1, jumboText2);
 
     // container for all portfolio items
     const portfolioContainer = $("<div>").addClass("container").appendTo(portfolioPage);
@@ -87,18 +95,25 @@ function displayPortfolioPage() {
     // item 1
     const portfolioCol1 = $("<div>").addClass("col-md-6").appendTo(portfolioRow1);
     const portfolioItem1 = $("<div>").addClass("media").appendTo(portfolioCol1);
-    const portfolioLink1 = $("<a>").attr("href", "https://jackryansmith.github.io/Group-work/").attr("target", "_blank").appendTo(portfolioItem1);
-    const portfolioPic1 = $("<img>").addClass("mr-3 border").attr("src", "./assets/group-project-img.jpg").attr("alt", "My Sports Page").appendTo(portfolioLink1);
-    const portfolioItemBody1 = $("<div>My Sports Page</div>").addClass("item-header").appendTo(portfolioItem1);
-    const portfolioItemText1 = $("<h5>").addClass("item-text").text("My first group project. My team came up with the idea for a sports info web app that called on iSports API to return local sports data.").appendTo(portfolioItemBody1);
+    const portfolioLink1 = $("<a>").attr("href", "https://jackryansmith.github.io/Group-work/").attr("target", "_blank");
+    const portfolioPic1 = $("<img>").addClass("mr-3 border").attr("src", "./assets/group-project-img.jpg").attr("alt", "My Sports Page");
+    portfolioLink1.append(portfolioPic1);
+    const portfolioItemBody1 = $("<div>My Sports Page</div>").addClass("item-header");
+    const portfolioItemText1 = $("<h5>").addClass("item-text").text("My first group project. My team came up with the idea for a sports info web app that called on iSports API to return local sports data.");
+    portfolioItemBody1.append(portfolioItemText1);
+    portfolioItem1.append(portfolioLink1, portfolioItemBody1);
 
     // item 2
     const portfolioCol2 = $("<div>").addClass("col-md-6").appendTo(portfolioRow1);
     const portfolioItem2 = $("<div>").addClass("media").appendTo(portfolioCol2);
-    const portfolioLink2 = $("<a>").attr("href", "https://dannyhoover.github.io/Weather-Dashboard/").attr("target", "_blank").appendTo(portfolioItem2);
-    const portfolioPic2 = $("<img>").addClass("mr-3 border").attr("src", "./assets/weather.img3.jpg").attr("alt", "Weather App").appendTo(portfolioLink2);
-    const portfolioItemBody2 = $("<div>Weather App</div>").addClass("item-header").appendTo(portfolioItem2);
-    const portfolioItemText2 = $("<h5>").addClass("item-text").text("This was a homework assignment where I called on the OpenWeather API to display current weather data and display a five day forecast of a city that the user chooses.").appendTo(portfolioItemBody2);
+    const portfolioLink2 = $("<a>").attr("href", "https://dannyhoover.github.io/Weather-Dashboard/").attr("target", "_blank");
+    const portfolioPic2 = $("<img>").addClass("mr-3 border").attr("src", "./assets/weather.img3.jpg").attr("alt", "Weather App");
+    portfolioLink2.append(portfolioPic2);
+    const portfolioItemBody2 = $("<div>Weather App</div>").addClass("item-header");
+    const portfolioItemText2 = $("<h5>").addClass("item-text").text("This was a homework assignment where I called on the OpenWeather API to display current weather data and display a five day forecast of a city that the user chooses.");
+    portfolioItemBody2.append(portfolioItemText2);
+    portfolioItem2.append(portfolioLink2, portfolioItemBody2);
+
 
     // row 2 containing next two items
     const portfolioRow2 = $("<div>").addClass("row").appendTo(portfolioContainer);
@@ -106,18 +121,24 @@ function displayPortfolioPage() {
     // item 3
     const portfolioCol3 = $("<div>").addClass("col-md-6").appendTo(portfolioRow2);
     const portfolioItem3 = $("<div>").addClass("media").appendTo(portfolioCol3);
-    const portfolioLink3 = $("<a>").attr("href", "https://dannyhoover.github.io/Calendar/02-Homework/Develop/").attr("target", "_blank").appendTo(portfolioItem3);
-    const portfolioPic3 = $("<img>").addClass("mr-3 border").attr("src", "./assets/calendar.img2.jpg").attr("alt", "Calendar App").appendTo(portfolioLink3);
-    const portfolioItemBody3 = $("<div>Calendar App</div>").addClass("item-header").appendTo(portfolioItem3);
-    const portfolioItemText3 = $("<h5>").addClass("item-text").text("A calendar app lets the user put in data for each hour of a normal work day and save the whole schedule.").appendTo(portfolioItemBody3);
+    const portfolioLink3 = $("<a>").attr("href", "https://dannyhoover.github.io/Calendar/02-Homework/Develop/").attr("target", "_blank");
+    const portfolioPic3 = $("<img>").addClass("mr-3 border").attr("src", "./assets/calendar.img2.jpg").attr("alt", "Calendar App");
+    portfolioLink3.append(portfolioPic3);
+    const portfolioItemBody3 = $("<div>Calendar App</div>").addClass("item-header");
+    const portfolioItemText3 = $("<h5>").addClass("item-text").text("A calendar app lets the user put in data for each hour of a normal work day and save the whole schedule.");
+    portfolioItemBody3.append(portfolioItemText3);
+    portfolioItem3.append(portfolioLink3, portfolioItemBody3);
 
     // item 4
     const portfolioCol4 = $("<div>").addClass("col-md-6").appendTo(portfolioRow2);
     const portfolioItem4 = $("<div>").addClass("media").appendTo(portfolioCol4);
-    const portfolioLink4 = $("<a>").attr("href", "https://dannyhoover.github.io/Homework3/").attr("target", "_blank").appendTo(portfolioItem4);
-    const portfolioPic4 = $("<img>").addClass("mr-3 border").attr("src", "./assets/passwordpic.jpg").attr("alt", "Password Generator").appendTo(portfolioLink4);
-    const portfolioItemBody4 = $("<div>Password Generator</div>").addClass("item-header").appendTo(portfolioItem4);
-    const portfolioItemText4 = $("<h5>").addClass("item-text").text("Generate a random password! Pick your characters, case, and length.").appendTo(portfolioItemBody4);
+    const portfolioLink4 = $("<a>").attr("href", "https://dannyhoover.github.io/Homework3/").attr("target", "_blank");
+    const portfolioPic4 = $("<img>").addClass("mr-3 border").attr("src", "./assets/passwordpic.jpg").attr("alt", "Password Generator");
+    portfolioLink4.append(portfolioPic4);
+    const portfolioItemBody4 = $("<div>Password Generator</div>").addClass("item-header");
+    const portfolioItemText4 = $("<h5>").addClass("item-text").text("Generate a random password! Pick your characters, case, and length.");
+    portfolioItemBody4.append(portfolioItemText4);
+    portfolioItem4.append(portfolioLink4, portfolioItemBody4);
 
     // // row 3 containing final two items
     // const portfolioRow3 = $("<div>").addClass("row").appendTo(portfolioContainer);
@@ -146,17 +167,21 @@ function displayContactPage() {
     document.getElementById("nav-item-contact").className = "nav-item active";
     const contactHeaderRow = $("<div>").addClass("row").appendTo(contactPage);
     const contactMeHeaderCol = $("<div>").addClass("col-md-3").appendTo(contactHeaderRow);
-    const contactMeHeaderText = $("<h1>").text("Contact Me: ").appendTo(contactMeHeaderCol);
+    const contactMeHeaderText = $("<h1>").text("Contact Me: ");
+    contactMeHeaderCol.append(contactMeHeaderText);
 
     // contact fields
     const contactPageRow1 = $("<div>").addClass("row").appendTo(contactPage);
     const nameDiv = $("<div>").addClass("col-md-6").text("Your name: ").appendTo(contactPageRow1);
-    const nameText = $("<textarea>").addClass("name-text").appendTo(nameDiv);
+    const nameText = $("<textarea>").addClass("name-text");
+    nameDiv.append(nameText);
     const emailDiv = $("<div>").addClass("col-md-6").text("Your Email: ").appendTo(contactPageRow1);
-    const emailText = $("<textarea>").addClass("email-text").appendTo(emailDiv);
+    const emailText = $("<textarea>").addClass("email-text");
+    emailDiv.append(emailText);
     const contactPageRow2 = $("<div>").addClass("row").appendTo(contactPage);
     const messageDiv = $("<div>").addClass("col-md-12").text("Your Message: ").appendTo(contactPageRow2);
-    const messageText = $("<textarea>").addClass("message-text").appendTo(messageDiv);
+    const messageText = $("<textarea>").addClass("message-text");
+    messageDiv.append(messageText)
 
     // submit button
     const submitBtnRow = $("<div>").addClass("row").appendTo(contactPage);
